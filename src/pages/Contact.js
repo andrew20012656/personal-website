bc3577350f56c4adf8e121f8ba6487b63fbba07c
andrew20012656 <andrew20012656@gmail.com>
2023-08-24 21:01:41 -0400

    Minor change - prepare to update Project page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,10 +7,7 @@ function Contact() {
         <div className="row align-items-center my-5">
           <h1 className="font-weight-light">About</h1>
           <p className="my-4">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            The project list is to be updated. Thank you.
           </p>
         </div>
       </div>
